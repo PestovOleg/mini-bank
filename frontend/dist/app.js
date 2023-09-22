e1943cb8134dd27bcd1ca9be772458133fb501bc
Pestov Oleg <pestoff.oleg@gmail.com>
2023-09-23 00:34:22 +0300

    init frontend

---
+++ frontend/dist/app.js
@@ -0,0 +1,2 @@
+import { FeatureToggles } from "./components/FeatureToggles.js";
+document.querySelector('app').innerHTML = FeatureToggles();
---
+++ frontend/src/app.ts
@@ -0,0 +1,3 @@
+import { FeatureToggles } from "./components/FeatureToggles.js";
+
+(document.querySelector('app') as HTMLElement).innerHTML=FeatureToggles();
